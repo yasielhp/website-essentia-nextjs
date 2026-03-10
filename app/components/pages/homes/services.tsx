c178d9c626856b7e8f46ddaaad2d8795754a306f
Yasiel Hernández Portal <yasielhp@gmail.com>
2026-03-10 19:06:22 +0000

    Add services section component

---
+++ app/components/pages/homes/services.tsx
@@ -0,0 +1,16 @@
+export default function Services() {
+  return (
+    <section
+      className="text-primary min-h-dvh overflow-hidden"
+      data-header-theme="dark"
+    >
+      <div className="flex h-dvh w-full flex-col justify-center gap-6 px-4 md:gap-14 md:px-8">
+        <div className="max-w-10xl mx-auto w-full">
+          <h2 className="font-display mt-20 px-6 text-center text-4xl md:mt-20 md:text-8xl">
+            Services
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
+}
