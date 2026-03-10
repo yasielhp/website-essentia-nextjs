a153e7a168a4f8e30cbf518a501f6ee26b799890
Yasiel Hernández Portal <yasielhp@gmail.com>
2026-03-10 19:06:26 +0000

    Add OurExperiences and Services sections to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import BrandStatement from "./components/pages/homes/brand-statement";
 import Hero from "./components/pages/homes/hero";
+import OurExperiences from "./components/pages/homes/our-experiences";
+import Services from "./components/pages/homes/services";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <BrandStatement />
+      <OurExperiences />
+      <Services />
     </>
   );
 }
