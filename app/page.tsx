ce2f76070ced3a460b9b62f1e8fa0946c955fcc9
Yasiel Hernández Portal <yasielhp@gmail.com>
2026-04-10 07:55:24 +0100

    Remove home sections and clean up page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-import BrandStatement from "./components/sections/homes/brand-statement";
-import Hero from "./components/sections/homes/hero";
-import OurExperiences from "./components/sections/homes/our-experiences";
-import Services from "./components/sections/homes/services";
-
 export default function Home() {
   return (
     <>
-      <Hero />
-      <BrandStatement />
-      <OurExperiences />
-      <Services />
+      <h1>Welcome to Our Website</h1>
     </>
   );
 }
