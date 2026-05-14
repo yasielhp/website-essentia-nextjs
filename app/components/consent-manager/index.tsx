1bce50cf892b951723e3a7499e1132fa215741b4
Yasiel Hernández Portal <yasielhp@gmail.com>
2026-05-14 12:22:16 +0100

    style: normalize quotes and formatting in consent-manager files

--- app/components/consent-manager/index.tsx
+++ app/components/consent-manager/index.tsx
@@ -1,14 +1,10 @@
-import type { ReactNode } from 'react';
-import ConsentManagerProvider from './provider';
+import type { ReactNode } from "react";
+import ConsentManagerProvider from "./provider";
 
 /**
  * Consent management wrapper.
  * @see https://c15t.com/docs/frameworks/nextjs/quickstart
  */
 export function ConsentManager({ children }: { children: ReactNode }) {
-	return (
-		<ConsentManagerProvider>
-			{children}
-		</ConsentManagerProvider>
-	);
+  return <ConsentManagerProvider>{children}</ConsentManagerProvider>;
 }
